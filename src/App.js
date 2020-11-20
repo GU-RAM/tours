d2eeedbb8c47ac933a8e880ed6e40eee44e90494
guro <guramidgebuadze@gmail.com>
2020-11-21 03:12:40 +0400

    one paged tours page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    setTours(tours.filter(tour => tour.id !== id));
+  }
+
+  const fetchTours = () => {
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        setTours(res);
+        setLoading(false);
+      },
+        (error) => {
+          setLoading(true);
+          console.log(error);
+        }
+      )
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
